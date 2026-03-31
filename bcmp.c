6ea8ba4f5ea1331e5bbfe0f195cd7861fb40dfed
Airton da Fonseca Granero <agranero@gmail.com>
2026-03-31 10:25:21 -0300

    Expanded a comment so even I undertand the decisions I made a few years from now.

--- bcmp.c
+++ bcmp.c
@@ -98,7 +98,12 @@ off_t parse_num(const char *str) {
         exit(2);
     }
 
-    // in some legacy 32-bit systems sizeof(OFF_T_MAX) < sizeof(unsigned long long)
+    // On 64-bit systems, off_t is typically a signed long long (64-bit).
+    // On legacy 32-bit systems (without _FILE_OFFSET_BITS=64), off_t may only be 32-bit.
+    // Since we use strtoull (always 64-bit unsigned long long ), the converted value might be ...
+    // ... valid for strtoull (not triggering ERANGE), but still exceed the local off_t capacity, ...
+    // ... either on 64 or 32 bit systems, so e added this check.
+    // We chose to maintain each system's native typing to ensure consistent behavior.
     if (val > OFF_T_MAX) {
         fprintf(stderr, "Error: '%s' overflows internal representation for counters.\n", str);
         exit(2);
